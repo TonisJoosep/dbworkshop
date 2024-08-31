f4600cf0b7cefe3a4256cfb5c8a2c336e4f2edde
Tonis Joosep <tonisjoosep@gmail.com>
2024-08-31 13:09:11 +0300

    Added new DB models

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from session import Session
-from model import Employee
+from model import Employee, Salary
 
 
 def main():
     session = Session()
 
-    query = session.query(Employee)
+    employee = session.query(Employee).get(1)
+    print(employee)
 
-    result = query.all()
-    print(result)
+    for salary in employee.salaries:
+        print(salary)
+
+    employee.salaries.append(
+        Salary(amount=50000, bonus=5000)
+    )
+    employee.pay(amount=1000, bonus=1000, comments="Monthly salary")
+    session.commit()
 
 
 if __name__ == "__main__":
--- session.py
+++ session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-URI = "mysql+mysqlconnector://root:qwerty@localhost:3306/startup"
+URI = "mysql+mysqlconnector://root:Vajaservuks225!@localhost:3306/startup"
 # URI = "sqlite:///startup.db"
 engine = create_engine(URI)
 Session = sessionmaker(bind=engine)
